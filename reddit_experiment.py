67ceb52c0c6cbac5a8dcc09a85ddc8b9956b8811
Høgni <hogniziska@gmail.com>
2021-05-07 10:26:36 +0200

    .

--- reddit_experiment.py
+++ reddit_experiment.py
@@ -5,7 +5,7 @@ import datetime
 from time import sleep
 from prawcore.exceptions import Forbidden
 
-number_of_posts = 100
+number_of_posts = 1100
 
 
 def user_login(client_id, client_secret, username, password, user_agent):
@@ -49,7 +49,7 @@ def get_onehundred_new_posts():
 
 
 try:
-    df = pd.read_pickle("reddit_experiment.pickle")
+    df = pd.read_pickle("reddit_experiment_with_comments.pickle")
 except FileNotFoundError:
     df = pd.DataFrame(
         columns=[
@@ -63,6 +63,13 @@ except FileNotFoundError:
             "day_4",
             "day_5",
             "day_6",
+            "day_0_comments",
+            "day_1_comments",
+            "day_2_comments",
+            "day_3_comments",
+            "day_4_comments",
+            "day_5_comments",
+            "day_6_comments",
         ]
     )
 
@@ -72,98 +79,123 @@ count_ex = 0
 count_con = 0
 error_list = []
 
-while count_ex != number_of_posts and count_con != number_of_posts:
-    if len(error_list) == 10:
-        break
-    print(f"New post progress {round(((count_ex + count_con)/(number_of_posts * 2))*100, 2)}%", end='\r')
-    posts = get_onehundred_new_posts()
-    for post_id in posts:
-        if post_id not in df["post_id"] and post_id not in df["post_id"]:
-            submission = reddit.submission(post_id)
-            if submission.score == 1:
-                if random.randint(0, 1) and count_ex != number_of_posts:
-                    upvote = upvote_single_post(post_id)
-                    if upvote != 0:
-                        df.loc[len(df)] = [today, post_id, "experiment", 2, 0, 0, 0, 0, 0, 0]
-                        count_ex += 1
+try:
+    loop = (min(df["start_date"]) + datetime.timedelta(days=6)) <= today or len(df["start_date"]) != 1800
+except:
+    loop = True
+
+if loop:
+    while count_ex != number_of_posts and count_con != number_of_posts:
+        if len(error_list) == 10:
+            break
+        print(f"New post progress {round(((count_ex + count_con)/(number_of_posts * 2))*100, 2)}%", end='\r')
+        posts = get_onehundred_new_posts()
+        for post_id in posts:
+            if post_id not in df["post_id"] and post_id not in df["post_id"]:
+                submission = reddit.submission(post_id)
+                com = submission.num_comments
+                if submission.score == 1 and com == 0:
+                    if random.randint(0, 1) and count_ex != number_of_posts:
+                        upvote = upvote_single_post(post_id)
+                        if upvote != 0:
+                            df.loc[len(df)] = [today, post_id, "experiment", 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                            count_ex += 1
+                            print(f"New post progress {round(((count_ex + count_con)/(number_of_posts * 2))*100, 2)}%", end='\r')
+                        else:
+                            error_list.append(0)
+                            print("Sleeping... Please wait.", end='\r')
+                            sleep(320)
+                    elif count_con != number_of_posts:
+                        df.loc[len(df)] = [today, post_id, "control", 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                        count_con += 1
                         print(f"New post progress {round(((count_ex + count_con)/(number_of_posts * 2))*100, 2)}%", end='\r')
-                    else:
-                        error_list.append(0)
-                        print("Sleeping... Please wait.", end='\r')
-                        sleep(320)
-                elif count_con != number_of_posts:
-                    df.loc[len(df)] = [today, post_id, "control", 1, 0, 0, 0, 0, 0, 0]
-                    count_con += 1
-                    print(f"New post progress {round(((count_ex + count_con)/(number_of_posts * 2))*100, 2)}%", end='\r')
 
-
-for index, row in df.iterrows():
-    print(f"Updating progress {round((index/(len(df. index))*100), 2)}%", end='\r')
-    if today <= (row["start_date"] + datetime.timedelta(days=6)):
-        try:
-            submission = reddit.submission(row["post_id"])
-        except:
-            print("Sleeping... Please wait.", end='\r')
-            sleep(320)
-            submission = reddit.submission(row["post_id"])
-        if (row["start_date"] + datetime.timedelta(days=1)) == today:
+if today <= (max(df["start_date"]) + datetime.timedelta(days=6)):
+    for index, row in df.iterrows():
+        print(f"Updating progress {round((index/(len(df. index))*100), 2)}%", end='\r')
+        if today <= (row["start_date"] + datetime.timedelta(days=6)):
             try:
-                df.loc[index, "day_1"] = submission.score
+                submission = reddit.submission(row["post_id"])
             except:
                 print("Sleeping... Please wait.", end='\r')
-                sleep(320)
+                sleep(60)
+                submission = reddit.submission(row["post_id"])
+            if (row["start_date"] + datetime.timedelta(days=1)) == today:
                 try:
                     df.loc[index, "day_1"] = submission.score
-                except Forbidden:
-                    df.loc[index, "day_1"] =  df.loc[index, "day_0"]
-        elif (row["start_date"] + datetime.timedelta(days=2)) == today:
-            try:
-                df.loc[index, "day_2"] = submission.score
-            except:
-                print("Sleeping... Please wait.", end='\r')
-                sleep(320)
+                    df.loc[index, "day_1_comments"] = submission.num_comments
+                except:
+                    print("Sleeping... Please wait.", end='\r')
+                    sleep(60)
+                    try:
+                        df.loc[index, "day_1"] = submission.score
+                        df.loc[index, "day_1_comments"] = submission.num_comments
+                    except Forbidden:
+                        df.loc[index, "day_1"] =  df.loc[index, "day_0"]
+                        df.loc[index, "day_1_comments"] =  df.loc[index, "day_0_comments"]
+            elif (row["start_date"] + datetime.timedelta(days=2)) == today:
                 try:
                     df.loc[index, "day_2"] = submission.score
-                except Forbidden:
-                    df.loc[index, "day_2"] =  df.loc[index, "day_1"]
-        elif (row["start_date"] + datetime.timedelta(days=3)) == today:
-            try:
-                df.loc[index, "day_3"] = submission.score
-            except:
-                print("Sleeping... Please wait.", end='\r')
-                sleep(320)
+                    df.loc[index, "day_2_comments"] = submission.num_comments
+                except:
+                    print("Sleeping... Please wait.", end='\r')
+                    sleep(60)
+                    try:
+                        df.loc[index, "day_2"] = submission.score
+                        df.loc[index, "day_2_comments"] = submission.num_comments
+                    except Forbidden:
+                        df.loc[index, "day_2"] =  df.loc[index, "day_1"]
+                        df.loc[index, "day_2_comments"] =  df.loc[index, "day_1_comments"]
+            elif (row["start_date"] + datetime.timedelta(days=3)) == today:
                 try:
                     df.loc[index, "day_3"] = submission.score
-                except Forbidden:
-                    df.loc[index, "day_3"] =  df.loc[index, "day_2"]
-        elif (row["start_date"] + datetime.timedelta(days=4)) == today:
-            try:
-                df.loc[index, "day_4"] = submission.score
-            except:
-                print("Sleeping... Please wait.", end='\r')
-                sleep(320)
+                    df.loc[index, "day_3_comments"] = submission.num_comments
+                except:
+                    print("Sleeping... Please wait.", end='\r')
+                    sleep(60)
+                    try:
+                        df.loc[index, "day_3"] = submission.score
+                        df.loc[index, "day_3_comments"] = submission.num_comments
+                    except Forbidden:
+                        df.loc[index, "day_3"] =  df.loc[index, "day_2"]
+                        df.loc[index, "day_3_comments"] =  df.loc[index, "day_2_comments"]
+            elif (row["start_date"] + datetime.timedelta(days=4)) == today:
                 try:
                     df.loc[index, "day_4"] = submission.score
-                except Forbidden:
-                    df.loc[index, "day_4"] =  df.loc[index, "day_3"]
-        elif (row["start_date"] + datetime.timedelta(days=5)) == today:
-            try:
-                df.loc[index, "day_5"] = submission.score
-            except:
-                print("Sleeping... Please wait.", end='\r')
-                sleep(320)
+                    df.loc[index, "day_4_comments"] = submission.num_comments
+                except:
+                    print("Sleeping... Please wait.", end='\r')
+                    sleep(60)
+                    try:
+                        df.loc[index, "day_4"] = submission.score
+                        df.loc[index, "day_4_comments"] = submission.num_comments
+                    except Forbidden:
+                        df.loc[index, "day_4"] =  df.loc[index, "day_3"]
+                        df.loc[index, "day_4_comments"] =  df.loc[index, "day_3_comments"]
+            elif (row["start_date"] + datetime.timedelta(days=5)) == today:
                 try:
                     df.loc[index, "day_5"] = submission.score
-                except Forbidden:
-                    df.loc[index, "day_5"] =  df.loc[index, "day_4"]
-        elif (row["start_date"] + datetime.timedelta(days=6)) == today:
-            try:
-                df.loc[index, "day_6"] = submission.score
-            except:
-                print("Sleeping... Please wait.", end='\r')
-                sleep(320)
+                    df.loc[index, "day_5_comments"] = submission.num_comments
+                except:
+                    print("Sleeping... Please wait.", end='\r')
+                    sleep(60)
+                    try:
+                        df.loc[index, "day_5"] = submission.score
+                        df.loc[index, "day_5_comments"] = submission.num_comments
+                    except Forbidden:
+                        df.loc[index, "day_5"] =  df.loc[index, "day_4"]
+                        df.loc[index, "day_5_comments"] =  df.loc[index, "day_4_comments"]
+            elif (row["start_date"] + datetime.timedelta(days=6)) == today:
                 try:
                     df.loc[index, "day_6"] = submission.score
-                except Forbidden:
-                    df.loc[index, "day_6"] =  df.loc[index, "day_5"]
-df.to_pickle("reddit_experiment.pickle")
+                    df.loc[index, "day_6_comments"] = submission.num_comments
+                except:
+                    print("Sleeping... Please wait.", end='\r')
+                    sleep(60)
+                    try:
+                        df.loc[index, "day_6"] = submission.score
+                        df.loc[index, "day_6_comments"] = submission.num_comments
+                    except Forbidden:
+                        df.loc[index, "day_6"] =  df.loc[index, "day_5"]
+                        df.loc[index, "day_6_comments"] =  df.loc[index, "day_5_comments"]
+df.to_pickle("reddit_experiment_with_comments.pickle")
--- view_pickle_data.py
+++ view_pickle_data.py
@@ -1,5 +1,18 @@
 import pandas as pd
+#import datetime
 
-df = pd.read_pickle("reddit_experiment.pickle")
-df
-df[0:50]
+df = pd.read_pickle("reddit_experiment_with_comments.pickle")
+df[1750:1800]
+# date = "2021-04-03"
+# date_time_obj = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+# date_time_obj += datetime.timedelta(days=7)
+# date_time_obj
+# df = df[df["start_date"] != date_time_obj]
+# df
+
+# min(df["start_date"])
+# for name, group in df.groupby("start_date"):
+#     print(name)
+#     print(len(group["start_date"]))
+
+# df.to_pickle("reddit_experiment.pickle")
